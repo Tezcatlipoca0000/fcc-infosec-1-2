ad32dc3b4a2bb22fdd9b816978181bab5d27fe43
Tezcatlipoca0000 <tezcatlipoca0000@gmail.com>
2023-01-23 17:11:10 -0600

    Infosec 2.1 - 2.3

--- server.js
+++ server.js
@@ -15,19 +15,30 @@ const bcrypt = require('bcrypt');
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 // Lesson 2: Hash and Compare Passwords Asynchronously
+// Async await
 (async () => {
-    const hash = await bcrypt.hash(myPlaintextPassword, saltRounds);
+    let hash = await bcrypt.hash(myPlaintextPassword, saltRounds);
     console.log('my hash', hash);
-    const result = await bcrypt.compare(myPlaintextPassword, hash);
-    const result2 = await bcrypt.compare(someOtherPlaintextPassword, hash);
+    let result = await bcrypt.compare(myPlaintextPassword, hash);
+    let result2 = await bcrypt.compare(someOtherPlaintextPassword, hash);
     console.log('the results', result, result2);
 })();
+
+// callbacks
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
-
+// Lesson 3: Hash and Compare Passwords Synchronously
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log('the hash', hash, 'the result', result);
 
 //END_SYNC
 
